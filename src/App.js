c8e74eda0cb18c959cbd494dda7390ffe9344abc
Arham <mohdarham0634@gmail.com>
2023-11-09 22:26:21 +0530

    initial app stucture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 
 
+// import Home from './routes/Home';
+// import { Routes, Route } from 'react-router-dom';
 
 
 
@@ -9,9 +11,56 @@ function App() {
     return (
         <>
 
+            <header className="header">
+                <div className="header__container">
+
+                    <div className="header__brand">Brand</div>
+
+                    <nav className="header__nav">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                                <span className="nav__item__icon"></span>
+                                <span className="nav__item__text">Home</span>
+                            </li>
+                            <li className="nav__item">
+                                <span className="nav__item__icon"></span>
+                                <span className="nav__item__text">Search</span>
+                            </li>
+                            <li className="nav__item">
+                                <span className="nav__item__icon"></span>
+                                <span className="nav__item__text">Chats</span>
+                            </li>
+                            <li className="nav__item">
+                                <span className="nav__item__icon"></span>
+                                <span className="nav__item__text">Bookmarks</span>
+                            </li>
+                            <li className="nav__item">
+                                <span className="nav__item__icon"></span>
+                                <span className="nav__item__text">Profile</span>
+                            </li>
+                        </ul>
+                    </nav>
+
+
+                </div>
+            </header>
+
+
+            <main>
+                    <h1>Main</h1>
+                    
+
+            </main>
 
 
 
+
+
+
+
+            {/* <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes> */}
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+
+        <Router>
+
+            <App />
+
+        </Router>
+
+    </React.StrictMode>
 );
 
