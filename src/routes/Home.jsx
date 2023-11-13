8a914de1eec14d3553facfded2d4d03976e73e2d
Arham <mohdarham0634@gmail.com>
2023-11-14 03:38:30 +0530

    ...

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import user1avatar from '../assets/jasonstotham.jpg'
 import user1post1 from '../assets/jasonstothampost1.jpg'
 
-import { OutlineHeart } from '../assets/CustomIcons';
-import { FillHeart } from '../assets/CustomIcons';
+import { OutlineHeart, FillHeart } from '../assets/CustomIcons';
 import { OutlineComment } from '../assets/CustomIcons';
 import { OutlineShare } from '../assets/CustomIcons';
+import { OutlineSave } from '../assets/CustomIcons';
 
 const Home = () => {
     return (
@@ -35,11 +35,15 @@ const Home = () => {
                     </div>
 
                     <div className="post__actions">
-                        <button className="post__like-btn"><OutlineHeart /></button>
-                        <button className="post__comment-btn"><OutlineComment /></button>
-                        <button className="post__share-btn"><OutlineShare /></button>
+                        <button className="post__actions__btn"><OutlineHeart /></button>
+                        <button className="post__actions__btn"><OutlineComment /></button>
+                        <button className="post__actions__btn"><OutlineShare /></button>
+                        <button className="post__actions__btn"><OutlineSave /></button>
                     </div>
 
+                    <div className="post__likes">
+                        4,548,258 Likes
+                    </div>
                 </article>
             </div>
 
