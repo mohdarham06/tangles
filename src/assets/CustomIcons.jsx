7d86acbf573290c81f301ff2d8a1499c0f0f4a1d
mohdarham06 <mohdarham0634@gmail.com>
2024-01-05 23:56:20 +0530

    Saperate Mobile Navigation Structure

--- src/assets/CustomIcons.jsx
+++ src/assets/CustomIcons.jsx
@@ -101,7 +101,7 @@ export const FillMessages = () => {
 
 
 
-
+// Remove/Close
 export const OutlineRemove = () => {
     return (
         <svg aria-label="Close" fill="currentColor" width="1em" height="1em" viewBox="0 0 24 24">
@@ -161,6 +161,7 @@ export const OutlineArtBrush = () => {
         </>
     )
 }
+
 export const OutlineLogout = () => {
     return (
         <>
@@ -171,6 +172,19 @@ export const OutlineLogout = () => {
         </>
     )
 }
+
+export const OutlineOptions = () => {
+    return (
+        <>
+            <svg aria-label="More" fill="currentColor" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24">
+                <title>More</title>
+                <g><path d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"></path></g>
+            </svg>
+        </>
+    )
+}
+
+
 
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import noUserImage from '../data/avatars/noimage.jpg'
 import {
     OutlineHome, OutlineSearch, OutlineNotification, OutlineSaved, OutlineFeatherPen,
     FillHome, OutlineBoldSearch, FillNotification, FillSaved,
+    OutlineOptions,
     OutlineArtBrush, OutlineLogout
 } from '../assets/CustomIcons'
 
@@ -15,69 +16,71 @@ const Header = ({ openPostModal, openDisplayModal }) => {
 
     return (
         <header className="header">
-            <div className="header__container">
+
+            {/* Header Desktop */}
+            <div className="header__desktop">
 
                 <Link
-                    className="header__brand"
+                    className="header__desktop__brand"
                     to={`/`}
                 >
-                    <span className="header__brand__icon"></span>
-                    <span className="header__brand__text"></span>
+                    <span className="header__desktop__brand__icon"></span>
+                    <span className="header__desktop__brand__text"></span>
                 </Link>
 
 
-                <nav className="header__nav">
-                    <ul className="nav__list">
+                <nav className="header__desktop__nav">
+                    <ul className="desktop__nav__list">
                         <NavLink to={`/`}>
-                            <li className="nav__item">
-                                <span className="nav__item__icon">
+                            <li className="desktop__nav__item">
+                                <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/' ? <OutlineHome /> : <FillHome />}</span>
-                                <span className="nav__item__text">Home</span>
+                                <span className="desktop__nav__item__text">Home</span>
                             </li>
                         </NavLink>
                         <NavLink to={`/search`}>
-                            <li className="nav__item">
-                                <span className="nav__item__icon">
+                            <li className="desktop__nav__item">
+                                <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/search' ? <OutlineSearch /> : <OutlineBoldSearch />}</span>
 
-                                <span className="nav__item__text">Search</span>
+                                <span className="desktop__nav__item__text">Search</span>
                             </li>
                         </NavLink>
                         <NavLink to={`/notifications`}>
-                            <li className="nav__item">
-                                <span className="nav__item__icon">
+                            <li className="desktop__nav__item">
+                                <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/notifications' ? <OutlineNotification /> : <FillNotification />}</span>
-                                <span className="nav__item__text">Notifications</span>
+                                <span className="desktop__nav__item__text">Notifications</span>
                             </li>
                         </NavLink>
                         <NavLink to={`/saved`}>
-                            <li className="nav__item">
-                                <span className="nav__item__icon">
+                            <li className="desktop__nav__item">
+                                <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/saved' ? <OutlineSaved /> : <FillSaved />}</span>
-                                <span className="nav__item__text">Saved</span>
+                                <span className="desktop__nav__item__text">Saved</span>
                             </li>
                         </NavLink>
 
                         <NavLink to={`/${"mohdarham"}`}>
-                            <li className="nav__item">
+                            <li className="desktop__nav__item">
                                 <img
-                                    className="nav__item__avatar"
+                                    className="desktop__nav__item__avatar"
                                     src={user1avatar ? user1avatar : noUserImage}
                                     alt=""
                                 />
-                                <span className="nav__item__text">Profile</span>
+                                <span className="desktop__nav__item__text">Profile</span>
                             </li>
                         </NavLink>
                     </ul>
                     <div onClick={openPostModal}>
-                        <div className="primary-button nav__post-btn">
-                            <span className="nav__post-btn__icon"><OutlineFeatherPen /></span>
-                            <span className="nav__post-btn__text">Post</span>
+                        <div className="primary-button desktop__nav__post-btn">
+                            <span className="desktop__nav__post-btn__icon"><OutlineFeatherPen /></span>
+                            <span className="desktop__nav__post-btn__text">Post</span>
                         </div>
                     </div>
                 </nav>
 
-                <div className="header__">
+                <div className="header__desktop__">
                     <div className="header__action-btn" onClick={openDisplayModal}>
                         <span className="header__action-btn__icon"><OutlineArtBrush /></span>
                         <span className="header__action-btn__text">Display</span>
@@ -90,6 +93,60 @@ const Header = ({ openPostModal, openDisplayModal }) => {
                 </div>
 
             </div>
+
+
+
+            {/* Header Mobile */}
+            <div className="header__mobile">
+
+                <div className="header__mobile__top">
+                    <Link
+                        className="header__mobile__brand"
+                        to={`/`}
+                    >
+                        <span className="header__mobile__brand__icon"></span>
+                        <span className="header__mobile__brand__text"></span>
+                    </Link>
+                </div>
+
+
+                <nav className="header__mobile__nav">
+                    <ul className="mobile__nav__list">
+                        <NavLink to={`/`}>
+                            <li className="mobile__nav__item">
+                                {location.pathname !== '/' ? <OutlineHome /> : <FillHome />}
+                            </li>
+                        </NavLink>
+                        <NavLink to={`/search`}>
+                            <li className="mobile__nav__item">
+                                {location.pathname !== '/search' ? <OutlineSearch /> : <OutlineBoldSearch />}
+                            </li>
+                        </NavLink>
+                        <NavLink to={`/notifications`}>
+                            <li className="mobile__nav__item">
+                                {location.pathname !== '/notifications' ? <OutlineNotification /> : <FillNotification />}
+                            </li>
+                        </NavLink>
+                        <NavLink to={`/saved`}>
+                            <li className="mobile__nav__item">
+                                {location.pathname !== '/saved' ? <OutlineSaved /> : <FillSaved />}
+                            </li>
+                        </NavLink>
+
+                        <NavLink to={`/${"mohdarham"}`}>
+                            <li className="mobile__nav__item">
+                                <img
+                                    className="mobile__nav__item__avatar"
+                                    src={user1avatar ? user1avatar : noUserImage}
+                                    alt=""
+                                />
+                            </li>
+                        </NavLink>
+                    </ul>
+                   
+                </nav>
+            </div>
+
         </header>
     )
 }
