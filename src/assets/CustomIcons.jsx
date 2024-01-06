d1ad4a4e203fc6f5db95ea0d2b3bec6aaf74d4db
mohdarham06 <mohdarham0634@gmail.com>
2024-01-06 20:59:20 +0530

    Modern Responive Mobile Navigation

--- src/assets/CustomIcons.jsx
+++ src/assets/CustomIcons.jsx
@@ -165,7 +165,7 @@ export const OutlineArtBrush = () => {
 export const OutlineLogout = () => {
     return (
         <>
-            <svg aria-label="Logout" stroke="currentColor" fill="currentColor" stroke-width="0" height="1em" width="1em" viewBox="0 0 24 24">
+            <svg aria-label="Logout" fill="currentColor" height="1em" width="1em" viewBox="0 0 24 24">
                 <title>Logout</title>
                 <path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path><path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path>
             </svg>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import user1avatar from '../data/avatars/jasonstotham.jpg'
@@ -13,6 +13,11 @@ import {
 
 const Header = ({ openPostModal, openDisplayModal }) => {
     const location = useLocation();
+    const [isMenuChecked, setIsMenuChecked] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuChecked(!isMenuChecked)
+    }
 
     return (
         <header className="header">
@@ -31,14 +36,14 @@ const Header = ({ openPostModal, openDisplayModal }) => {
 
                 <nav className="header__desktop__nav">
                     <ul className="desktop__nav__list">
-                        <NavLink to={`/`}>
+                        <NavLink to={`/`} className="desktop__nav__link">
                             <li className="desktop__nav__item">
                                 <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/' ? <OutlineHome /> : <FillHome />}</span>
                                 <span className="desktop__nav__item__text">Home</span>
                             </li>
                         </NavLink>
-                        <NavLink to={`/search`}>
+                        <NavLink to={`/search`} className="desktop__nav__link">
                             <li className="desktop__nav__item">
                                 <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/search' ? <OutlineSearch /> : <OutlineBoldSearch />}</span>
@@ -46,14 +51,14 @@ const Header = ({ openPostModal, openDisplayModal }) => {
                                 <span className="desktop__nav__item__text">Search</span>
                             </li>
                         </NavLink>
-                        <NavLink to={`/notifications`}>
+                        <NavLink to={`/notifications`} className="desktop__nav__link">
                             <li className="desktop__nav__item">
                                 <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/notifications' ? <OutlineNotification /> : <FillNotification />}</span>
                                 <span className="desktop__nav__item__text">Notifications</span>
                             </li>
                         </NavLink>
-                        <NavLink to={`/saved`}>
+                        <NavLink to={`/saved`} className="desktop__nav__link">
                             <li className="desktop__nav__item">
                                 <span className="desktop__nav__item__icon">
                                     {location.pathname !== '/saved' ? <OutlineSaved /> : <FillSaved />}</span>
@@ -61,7 +66,7 @@ const Header = ({ openPostModal, openDisplayModal }) => {
                             </li>
                         </NavLink>
 
-                        <NavLink to={`/${"mohdarham"}`}>
+                        <NavLink to={`/${"mohdarham"}`} className="desktop__nav__link">
                             <li className="desktop__nav__item">
                                 <img
                                     className="desktop__nav__item__avatar"
@@ -80,14 +85,27 @@ const Header = ({ openPostModal, openDisplayModal }) => {
                     </div>
                 </nav>
 
-                <div className="header__desktop__">
-                    <div className="header__action-btn" onClick={openDisplayModal}>
-                        <span className="header__action-btn__icon"><OutlineArtBrush /></span>
-                        <span className="header__action-btn__text">Display</span>
+                <label htmlFor="menu-checkbox" className="header__desktop__menu-btn">
+                    <input
+                        id="menu-checkbox"
+                        type="checkbox"
+                        checked={isMenuChecked}
+                        onChange={toggleMenu}
+                    ></input>
+
+                    <div className="header__desktop__menu-btn__icon"><OutlineOptions /></div>
+                    <div className="header__desktop__menu-btn__text">More</div>
+                </label>
+
+
+                <div className={`header__desktop__options ${isMenuChecked ? 'visible' : 'hidden'}`}>
+                    <div className="desktop__options__action-btn" onClick={openDisplayModal}>
+                        <span className="desktop__options__action-btn__icon"><OutlineArtBrush /></span>
+                        <span className="desktop__options__action-btn__text">Display</span>
                     </div>
-                    <div className="header__action-btn header__logout">
-                        <span className="header__action-btn__icon"><OutlineLogout /></span>
-                        <span className="header__action-btn__text">Logout</span>
+                    <div className="desktop__options__action-btn header__logout">
+                        <span className="desktop__options__action-btn__icon"><OutlineLogout /></span>
+                        <span className="desktop__options__action-btn__text">Log out</span>
 
                     </div>
                 </div>
@@ -96,44 +114,71 @@ const Header = ({ openPostModal, openDisplayModal }) => {
 
 
 
+
             {/* Header Mobile */}
             <div className="header__mobile">
 
                 <div className="header__mobile__top">
+                    <div></div>
+
                     <Link
                         className="header__mobile__brand"
                         to={`/`}
                     >
                         <span className="header__mobile__brand__icon"></span>
-                        <span className="header__mobile__brand__text"></span>
+                        <span className="header__mobile__brand__text">Tangles</span>
                     </Link>
+
+                    <label htmlFor="menu-checkbox" className="header__mobile__menu-btn">
+                        <input
+                            id="menu-checkbox"
+                            type="checkbox"
+                            checked={isMenuChecked}
+                            onChange={toggleMenu}
+                        ></input>
+                        <span className="menu-bar menu-bar--top"></span>
+                        <span className="menu-bar menu-bar--mid"></span>
+                    </label>
+
+
+                    <div className={`header__mobile__options ${isMenuChecked ? 'visible' : 'hidden'}`}>
+                        <div className="mobile__options__action-btn"
+                            onClick={openDisplayModal}
+                        >
+                            <span className="mobile__options__action-btn__icon"><OutlineArtBrush /></span>
+                            <span className="mobile__options__action-btn__text">Display</span>
+                        </div>
+                        <div className="mobile__options__action-btn header__logout">
+                            <span className="mobile__options__action-btn__icon"><OutlineLogout /></span>
+                            <span className="mobile__options__action-btn__text">Log out</span>
+                        </div>
+                    </div>
                 </div>
 
 
                 <nav className="header__mobile__nav">
                     <ul className="mobile__nav__list">
-                        <NavLink to={`/`}>
+                        <NavLink to={`/`} className="mobile__nav__link">
                             <li className="mobile__nav__item">
                                 {location.pathname !== '/' ? <OutlineHome /> : <FillHome />}
                             </li>
                         </NavLink>
-                        <NavLink to={`/search`}>
+                        <NavLink to={`/search`} className="mobile__nav__link">
                             <li className="mobile__nav__item">
                                 {location.pathname !== '/search' ? <OutlineSearch /> : <OutlineBoldSearch />}
                             </li>
                         </NavLink>
-                        <NavLink to={`/notifications`}>
+                        <NavLink to={`/notifications`} className="mobile__nav__link">
                             <li className="mobile__nav__item">
                                 {location.pathname !== '/notifications' ? <OutlineNotification /> : <FillNotification />}
                             </li>
                         </NavLink>
-                        <NavLink to={`/saved`}>
+                        <NavLink to={`/saved`} className="mobile__nav__link">
                             <li className="mobile__nav__item">
                                 {location.pathname !== '/saved' ? <OutlineSaved /> : <FillSaved />}
                             </li>
                         </NavLink>
-
-                        <NavLink to={`/${"mohdarham"}`}>
+                        <NavLink to={`/${"mohdarham"}`} className="mobile__nav__link">
                             <li className="mobile__nav__item">
                                 <img
                                     className="mobile__nav__item__avatar"
@@ -143,7 +188,7 @@ const Header = ({ openPostModal, openDisplayModal }) => {
                             </li>
                         </NavLink>
                     </ul>
-                   
+
                 </nav>
             </div>
 
